7af69084d588be1c3e2f7298f66025dd0c211a6c
LuisaRom <lufromero13@gmail.com>
2025-02-18 11:39:38 -0500

    Datos Luisa

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Luisa Romero");
+        System.out.println("832652");
+
+
+
+
+        System.out.println("git config --global user.name");
+        System.out.println("git config --global user.gmail");
+        System.out.println("git config --list");
+        System.out.println("git init");
+        System.out.println("git status");
+        System.out.println("git long");
+        System.out.println("git add .");
+        System.out.println("git commit");
+        System.out.println("git push");
+        System.out.println("git pull");
+        System.out.println("git switch");
+        System.out.println("git branch -D");
+        System.out.println("gir branch --delete");
+        System.out.println("como devolver cambios de la rama");
+        System.out.println("git merge");
+
+    }
+}
